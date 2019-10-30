6ef55ba8b9749ea9a795fd66d2f718c9a93c5f9e
Mosima Damentt MAMALEKA <mmamalek@c4r8s8.wethinkcode.co.za>
2019-10-30 18:59:29 +0200

    verification code works

--- redirect_here.php
+++ redirect_here.php
@@ -1,58 +1,26 @@
 <?php
-echo $_GET['ver_code'];
-$var = '<script>alert("Hey you");</script>';
 
-echo filter_var($var, FILTER_SANITIZE_STRIPPED);
+require('connection.php');
+$code = $_GET['ver_code'];
 
-?>
+$sql = "SELECT * FROM `camagru`.`users` WHERE `verification_code` = ?";
+$stmt = $conn->prepare($sql);
 
+$stmt->execute([$code]);
 
-<?php
-	require('connection.php');
+$results = $stmt->fetch(PDO::FETCH_ASSOC);
 
-	$username = $_GET['username'];
-	$email = $_GET['email'];
+if ($results)
+{
+	echo "Welcome to CamagUru";
+	$sql = "UPDATE `users` SET `verified` = '1' WHERE `users`.`verification_code` = ?";
+	$stmt = $conn->prepare($sql);
+	$stmt->execute([$code]);
+	$sql = "UPDATE `users` SET `verification_code` = '' WHERE `users`.`verification_code` = ?";
+	$stmt = $conn->prepare($sql);
+	$stmt->execute([$code]);
 
-	$exists = FALSE;
-	
-	try{
-		$sql = "SELECT * FROM users WHERE username = ? && email = ?";
-		$stmt = $conn->prepare($sql);
-		$stmt->execute([$username, $email]);
-		$results = $stmt->fetch(PDO::FETCH_ASSOC);
-
-
-		if ($results)
-		{
-			$exists = TRUE;
-		}
-		else
-		{
-			echo "Link did not work.";
-		}
-	} catch (PDOException $e){
-		echo $e->getMessage();
-	} 
+}
 
 
-
-	if (!$exists){
-
-		try{
-			$sql = 'INSERT INTO users (username, passwd, email, notifications, verification_code) VALUES (?, ?, ?, 0, ?)';
-			$stmt = $conn->prepare($sql);
-			$stmt->execute([$username, $password, $email, $verification_code]);
-			echo 'seccessfull <br />';
-			
-			$msg= "click the link to verify your account: http://127.0.0.1:8080/redirect_here.php?ver_code=$verification_code";
-			$headers = array(
-				'From: noreply');
-
-			mail($email, "Verification email", $msg, implode("\r\n",$headers));
-			echo "<br />Check your email ";
-
-		} catch (PDOException $e){
-			echo $e->getMessage();
-		}
-	}
 ?>
